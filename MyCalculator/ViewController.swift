002eaf95eae43fe9e885cbd8acb97eec21acb9b0
Chisom <chiggyokwor@gmail.com>
2020-04-14 22:10:14 -0400

    Initial Commit

---
+++ MyCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCalculator
+//
+//  Created by Chisom Okwor on 4/14/20.
+//  Copyright © 2020 Chisom Okwor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
